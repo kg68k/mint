9d5aa586b5eb85c96367b181f5bc8538cd1a8e3a
kg68k <78926718+kg68k@users.noreply.github.com>
2024-06-04 02:00:21 +0900

    perf: share the tail of built-in function names

--- src/mkfunctbl.c
+++ src/mkfunctbl.c
@@ -2,6 +2,7 @@
 // Copyright (C) 2024 TcbnErik
 
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +16,10 @@
 typedef struct {
   const char* name;
   const char* label;
+  const char* reverse;
+  uint8_t no;
+  uint8_t unifyLength;
+  uint8_t unifyNo;
 } Func;
 
 static int funcsLen;
@@ -33,6 +38,22 @@ static char* Strdup(const char* s) {
   char* t = strdup(s);
   if (!t) Error("メモリが不足しています。\n");
   return t;
+}
+
+static char* ReverseString(char* s) {
+  size_t len = strlen(s);
+  size_t n = len / 2;
+  char* left = s;
+  char* right = s + len;
+  size_t i;
+
+  for (i = 0; i < n; i += 1) {
+    char c = *left;
+    *left++ = *--right;
+    *right = c;
+  }
+
+  return s;
 }
 
 static char* Fgets(char* buf, size_t size, FILE* fp) {
@@ -83,23 +104,69 @@ static int compareFuncName(const void* a, const void* b) {
 static void ReadLines(const char* filename, FILE* fp) {
   char buf[256];
   int line = 1;
+  int i;
 
   while (Fgets(buf, sizeof(buf), fp)) {
     const char* label = extractLabel(buf);
     const char* name = labelToKey(label);
+    const char* reverse;
 
     if (!label || !name) continue;
 
     if (funcsLen == FUNC_MAX)
       Error("%s: %d: 内部関数が多すぎます: \n", filename, line, buf);
 
-    funcs[funcsLen] = (Func){name, label};
+    reverse = ReverseString(Strdup(name));
+
+    funcs[funcsLen] = (Func){name, label, reverse, 0, 0, 0};
     funcsLen += 1;
   }
 
   if (funcsLen == 0) Error("%s: 内部関数がありません。\n", filename);
 
   qsort(funcs, funcsLen, sizeof(funcs[0]), compareFuncName);
+
+  for (i = 0; i < funcsLen; i += 1) {
+    funcs[i].no = i;
+  }
+}
+
+// 左右反転した文字列を逆順でソートするための比較関数
+static int compareFuncReverse(const void* a, const void* b) {
+  return strcmp(((const Func*)b)->reverse, ((const Func*)a)->reverse);
+}
+
+static int compareFuncNo(const void* a, const void* b) {
+  return ((const Func*)a)->no - ((const Func*)b)->no;
+}
+
+static int getPreviousReverseIndex(int index) {
+  index -= 1;
+  while (funcs[index].unifyLength != 0) {
+    index = funcs[index].unifyNo;
+  }
+  return index;
+}
+
+static void UnifyCommonTail(void) {
+  int i;
+
+  // 左右反転した文字列の逆順に並び替える
+  qsort(funcs, funcsLen, sizeof(funcs[0]), compareFuncReverse);
+
+  // funcs[0] には直前の関数名がないので [1] からはじめる
+  for (i = 1; i < funcsLen; i += 1) {
+    int prevIndex = getPreviousReverseIndex(i);
+    size_t len = strlen(funcs[i].reverse);
+
+    if (memcmp(funcs[prevIndex].reverse, funcs[i].reverse, len) == 0) {
+      funcs[i].unifyLength = len;
+      funcs[i].unifyNo = funcs[prevIndex].no;
+    }
+  }
+
+  // 元の順に戻す
+  qsort(funcs, funcsLen, sizeof(funcs[0]), compareFuncNo);
 }
 
 static void outputFuncLabel(void) {
@@ -118,7 +185,15 @@ static void outputFuncName(void) {
 
   puts("\nFuncNameList:\nS:");
   for (i = 0; i < funcsLen; i += 1) {
-    printf("s%03d: .dc.b '%s',0\n", i, funcs[i].name);
+    int unifyLen = funcs[i].unifyLength;
+    if (unifyLen == 0) {
+      printf("s%03d: .dc.b '%s',0\n", i, funcs[i].name);
+    } else {
+      const char* unifyName = funcs[funcs[i].unifyNo].name;
+      int skip = strlen(unifyName) - unifyLen;
+      printf("s%03d: .equ s%03d+%d  ;%s\n", i, funcs[i].unifyNo, skip,
+             funcs[i].name);
+    }
   }
 }
 
@@ -136,6 +211,7 @@ static void Output(void) {
 
 int main(void) {
   ReadLines("<stdin>", stdin);
+  UnifyCommonTail();
   Output();
 
   return EXIT_SUCCESS;
